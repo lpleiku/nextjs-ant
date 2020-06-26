8079508f7be5a7f89a41b6b6f2ffbaa2f8890255
lpleiku <vuducnam2711@gmail.com>
2020-06-26 16:16:06 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="stylesheet" href="/antd.min.css">
+        </Head>
         <body>
           <Main />
           <NextScript />
